6ece77b4de21f6299ca34f26ca6e9bf0d19ac17e
Ozcan <murat.ozcan@siemens.com>
2020-05-30 15:28:20 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  extends: ['plugin:@angular-eslint/recommended'],
+  rules: {
+  // ORIGINAL tslint.json -> "directive-selector": [true, "attribute", "app", "camelCase"],
+    '@angular-eslint/directive-selector': [
+      'error',
+      { type: 'attribute', prefix: 'app', style: 'camelCase' },
+    ],
+    // ORIGINAL tslint.json -> "component-selector": [true, "element", "app", "kebab-case"],
+    '@angular-eslint/component-selector': [
+      'error',
+      { type: 'element', prefix: 'app', style: 'kebab-case' },
+    ],
+  },
+  overrides: [
+    // Add these rules, if you use inline templates inside *.component.ts files
+    {
+      files: ['*.component.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      plugins: ['@angular-eslint/template'],
+      processor: '@angular-eslint/template/extract-inline-html',
+    },
+    {
+      files: ['*.ts'],
+      extends: [
+        // AirBnB Styleguide rules
+        'airbnb-typescript/base',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    }
+  ],
+};
